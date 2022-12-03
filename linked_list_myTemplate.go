103287273a94c26f4fa16a4c8398f44bcc37d407
champ <kkongsil@outlook.com>
2022-12-03 22:12:24 +0700

    linked_list_myTemplate.go

---
+++ linked_list_myTemplate.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type List struct {
+	Head *MyNode
+}
+
+type MyNode struct {
+	Value interface{}
+	Next  *MyNode
+}
+
+func (l *List) Len() int {
+	p := l.Head
+	var i int = 0
+	// add => add => nil
+	for p.Next != nil {
+		p = p.Next
+		i++
+		if p.Next == nil {
+			i++
+		}
+	}
+	return i
+}
+
+func (l *List) Push(value interface{}) {
+	n := &MyNode{value, nil}
+	if l.Head == nil {
+		l.Head = n
+	} else {
+		w := l.Head
+		for w.Next != nil {
+			w = w.Next
+		}
+		w.Next = n
+	}
+}
+
+func (l *List) Print() {
+	p := l.Head
+	for p != nil {
+		fmt.Println(p.Value)
+		p = p.Next
+	}
+}
+
+func main() {
+	l := List{nil}
+	l.Push(0)
+	l.Push(9)
+	l.Push(20)
+	l.Print()
+}

714c53f0ee510b74339562b28829c14c210a6799
champ <kkongsil@outlook.com>
2022-12-09 22:50:46 +0700

    d6: stock span problem.

--- d6.go
+++ d6.go
@@ -33,12 +33,53 @@ func (s *Stack) Peak() int {
 	}
 }
 
-func solveProblem(stack Stack) {
-	fmt.Println(stack.Peak())
+func solveProblem(stack Stack, prices []int) {
+	// fmt.Println(stack.Peak())
+	//
+	// var currentValue int = prices[0]
+	// var span []int // slice declaration; no memory allocation
+	var span = make([]int, 0)
+	//slice initialization with length (0) and capacity (10);
+	//memory allocated for 10 ints
+	stack.Push(0)
 
+	for i := 0; i < len(prices); i++ {
+		span = append(span, 0)
+	}
+
+	span[0] = 1
+
+	for i := 0; i < len(prices); i++ {
+		for {
+			if !stack.IsEmpty() {
+				if prices[stack.Peak()] <= prices[i] {
+					stack.Pop()
+				} else {
+					break
+				}
+			} else {
+				break
+			}
+		}
+		//
+
+		if stack.IsEmpty() {
+			// Case: 1
+			span[i] = i + 1
+		} else {
+			// Case: 2
+			span[i] = i - stack.Peak()
+		}
+
+		stack.Push(i)
+	}
+
+	fmt.Println(span)
 }
 
 func main() {
 	var stack Stack
-	// solveProblem(&stack)
+	var prices []int = []int{90, 40, 20, 30, 80, 60, 100}
+	// &stack
+	solveProblem(stack, prices)
 }
